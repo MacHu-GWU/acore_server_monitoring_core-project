3f358c3838c9b4f56731f3fc1f6a3e603ddc7e10
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-29 14:58:43 -0400

    [no ci]

--- acore_server_monitoring_core/api.py
+++ acore_server_monitoring_core/api.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+"""
+Usage example::
+
+    import acore_server_monitoring_core.api as acore_server_monitoring_core
+"""
+
 from .dynamodb import make_dynamodb_table_name
 from .dynamodb import UseCaseEnum
 from .dynamodb import Measurement
