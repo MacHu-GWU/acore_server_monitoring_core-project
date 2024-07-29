1b524a52c4fbff11e5d6699b1bbb0b3f8818077b
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-29 14:53:10 -0400

    0.1.2

--- acore_server_monitoring_core/dynamodb.py
+++ acore_server_monitoring_core/dynamodb.py
@@ -25,28 +25,21 @@ class Measurement(pm.Model):
         will automatically delete the expired data in a few days.
     """
 
-    series_id: pm.REQUIRED_STR = pm.NumberAttribute(hash_key=True)
-    create_at: pm.REQUIRED_DATETIME = pm.NumberAttribute(range_key=True)
+    series_id: pm.REQUIRED_STR = pm.UnicodeAttribute(hash_key=True)
+    create_at: pm.REQUIRED_DATETIME = pm.UTCDateTimeAttribute(range_key=True)
     expire_at: pm.REQUIRED_DATETIME = pm.TTLAttribute()
-    is_ec2_exists: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    is_rds_exists: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    is_ec2_running: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    is_rds_running: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    ec2_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None)
-    rds_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None)
-    connected_players: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    characters_in_world: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    server_uptime: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    cpu_usage: pm.OPTIONAL_FLOAT = pm.NumberAttribute(default=None)
-    memory_usage: pm.OPTIONAL_FLOAT = pm.NumberAttribute(default=None)
-    total_memory: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    available_memory: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
 
 
 class WorldServerStatusMeasurement(Measurement):
     """
     World server status measurement data.
 
+    :param is_ec2_exists: is EC2 exists.
+    :param is_rds_exists: is RDS exists.
+    :param is_ec2_running: is EC2 running.
+    :param is_rds_running: is RDS running.
+    :param ec2_status: EC2 status in string.
+    :param rds_status: RDS status in string.
     :param connected_players: connected players.
     :param characters_in_world: characters in world.
     :param server_uptime: server uptime in seconds.
@@ -56,30 +49,36 @@ class WorldServerStatusMeasurement(Measurement):
     :param available_memory: available memory in MB.
     """
 
-    connected_players: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    characters_in_world: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    server_uptime: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    cpu_usage: pm.OPTIONAL_FLOAT = pm.NumberAttribute(default=None)
-    memory_usage: pm.OPTIONAL_FLOAT = pm.NumberAttribute(default=None)
-    total_memory: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
-    available_memory: pm.OPTIONAL_INT = pm.NumberAttribute(default=None)
+    is_ec2_exists: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    is_rds_exists: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    is_ec2_running: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    is_rds_running: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    ec2_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None, null=True)
+    rds_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None, null=True)
+    connected_players: pm.OPTIONAL_INT = pm.NumberAttribute(default=None, null=True)
+    characters_in_world: pm.OPTIONAL_INT = pm.NumberAttribute(default=None, null=True)
+    server_uptime: pm.OPTIONAL_INT = pm.NumberAttribute(default=None, null=True)
+    cpu_usage: pm.OPTIONAL_FLOAT = pm.NumberAttribute(default=None, null=True)
+    memory_usage: pm.OPTIONAL_FLOAT = pm.NumberAttribute(default=None, null=True)
+    total_memory: pm.OPTIONAL_INT = pm.NumberAttribute(default=None, null=True)
+    available_memory: pm.OPTIONAL_INT = pm.NumberAttribute(default=None, null=True)
 
 
 class Ec2RdsStatusMeasurement(Measurement):
     """
     EC2 and RDS status measurement data.
 
-    :param is_ec2_exists:
-    :param is_rds_exists:
-    :param is_ec2_running:
-    :param is_rds_running:
-    :param ec2_status:
-    :param rds_status:
+    :param is_ec2_exists: is EC2 exists.
+    :param is_rds_exists: is RDS exists.
+    :param is_ec2_running: is EC2 running.
+    :param is_rds_running: is RDS running.
+    :param ec2_status: EC2 status in string.
+    :param rds_status: RDS status in string.
     """
 
-    is_ec2_exists: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    is_rds_exists: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    is_ec2_running: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    is_rds_running: pm.OPTIONAL_BINARY = pm.BooleanAttribute(default=None)
-    ec2_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None)
-    rds_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None)
+    is_ec2_exists: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    is_rds_exists: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    is_ec2_running: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    is_rds_running: pm.OPTIONAL_BOOL = pm.BooleanAttribute(default=None, null=True)
+    ec2_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None, null=True)
+    rds_status: pm.OPTIONAL_STR = pm.UnicodeAttribute(default=None, null=True)
